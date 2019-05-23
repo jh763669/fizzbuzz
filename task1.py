65139283d95a0955ccd386d487344ce7d1c64513
jh763669 <jh763669@dal.ca>
2019-05-23 11:32:20 -0300

    task

--- task1.py
+++ task1.py
@@ -1,14 +1,32 @@
 import re
 
-def l33t(page):
+def l33t(line):
+        line = re.sub('er','xor',line)
+        line_new = line.replace('o','0').replace('O','0').replace('a','4').replace('A','4').replace('e','3').replace('E','3').replace('i','1').replace('I','1')
+        return line_new
+
+
+temp = 1
+while temp == 1:
+    try:
+        filename_input = input('file name:')
+        fin = open(filename_input)
+    except:
+        print('no this file')
+    else:
+        temp = 2
+page = input('input page:')
+
+
+
+t = 1
+while t == 1:
     if page == '1':
         sum = 1
         fin1 = open(filename_input,'r')
         line = fin1.readline()
         line.strip()
-        line = re.sub('er','xor',line)
-        #line_new = line.replace
-        line_new = line.replace('o','0').replace('O','0').replace('a','4').replace('A','4').replace('e','3').replace('E','3').replace('i','1').replace('I','1')
+        line_new = l33t(line)
         #print(line,end='')
         fin2 = open('output.txt','a')
         fin2.write(line_new)
@@ -17,9 +35,8 @@ def l33t(page):
         for line in fin1:
             if sum < 25:
                 #print(line,end='')
+                line_new = l33t(line)
                 fin2 = open('output.txt','a')
-                line = re.sub('er','xor',line)
-                line_new = line.replace('o','0').replace('O','0').replace('a','4').replace('A','4').replace('e','3').replace('E','3').replace('i','1').replace('I','1')
                 fin2.write(line_new)
                 sum += 1
                 fin2.close()
@@ -36,30 +53,13 @@ def l33t(page):
         for line in fin1:
             if sum < 25:
                 #print(line,end='')
+                line_new = l33t(line)
                 fin2 = open('output.txt','a')
-                line = re.sub('er','xor',line)
-                line_new = line.replace('o','0').replace('O','0').replace('a','4').replace('A','4').replace('e','3').replace('E','3').replace('i','1').replace('I','1')
                 fin2.write(line_new)
                 sum += 1
                 fin2.close()
             else:
                 break
-
-
-temp = 1
-while temp == 1:
-    try:
-        filename_input = input('file name:')
-        fin = open(filename_input)
-    except:
-        print('no this file')
-    else:
-        temp = 2
-page = input('input page:')
-
-t = 1
-while t == 1:
-    l33t(page)
     while True:
         judge = input('do you want to continue? (y/n)')
         if judge == 'y':
