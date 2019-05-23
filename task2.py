9ef1336a7dffab774120df97e050c16f5841131b
jh763669 <jh763669@dal.ca>
2019-05-23 11:51:31 -0300

    task

--- task2.py
+++ task2.py
@@ -1,5 +1,5 @@
 import os
-import task1.py
+#from task1.py import l33t
 
 def get_file(root_path,all_files=[]):
     files = os.listdir(root_path)
@@ -24,18 +24,41 @@ for file in file_list:
         file_dict[file_split[-2]].append(temp)
 print(file_dict)
 
-fin = open('file.list','a')
+fin1 = open('file.list','a')
 for file in file_list:
     file_split = file.split('/')
     temp1 = file_split[-1]
     temp2 = file_split[-2]
     if file_split[-1] == '.DS_Store':
         continue
-    fin.write(file_split[-1])
-    fin.write(' - ')
-    fin.write(file_split[-2])
-    fin.write(' - ')
-    fin.write(str(len(file_split[-1])+len(file_split[-2])))
-    fin.write('\n')
-fin.close()
+    fin1.write(file_split[-1])
+    fin1.write(' - ')
+    fin1.write(file_split[-2])
+    fin1.write(' - ')
+    fin1.write(str(len(file_split[-1])+len(file_split[-2])))
+    fin1.write('\n')
+fin1.close()
+
+fin2 = open('folder.list','a')
+for file in file_list:
+    file_split = file.split('/')
+    #temp1 = file_split[-1]
+    #temp2 = file_split[-2]
+    print(file_split)
+    t1 = 0
+    for ele in file_split:
+        if ele != 'fizzbuzz-yourname':
+            t1 += 1
+        else:
+            break
+    t2 = len(file_split)-1
+    #print(t1,t2)
+    if file_split[-1] == '.DS_Store':
+        continue
+    if t2 - t1 == 1:
+        continue
+#    elif t2 - t1 > 1:
+#        t = t2 - t1
+#        for i in range(t):
+        
 
